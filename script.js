00c946e810cc62c660856243158dd2ffe713a173
Karolina <kika.romanska@gmail.com>
2018-01-09 15:44:58 +0100

    Clear all and check all buttons added

--- script.js
+++ script.js
@@ -25,3 +25,18 @@ function handleCheck(e) {
 }
 
 checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
+
+const checkAllButton = document.getElementById('checkAllButton');
+
+function checkAll(){
+    checkboxes.forEach(checkbox => checkbox.checked = true);
+}
+checkAllButton.addEventListener('click', checkAll);
+
+const clearAllButton = document.getElementById('clearAll');
+
+function clearAll(){
+    checkboxes.forEach(checkbox => checkbox.checked = false);
+}
+
+clearAllButton.addEventListener('click', clearAll);
